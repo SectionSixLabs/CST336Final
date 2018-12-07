918e2c0b4b37e98ec1c965da32c1d976ef06b8db
Lacey Sikes <lsikes@csumb.edu>
2018-12-07 17:24:45 +0000

    chickenen changes to header, proper highlights now in effect

--- cart.php
+++ cart.php
@@ -28,6 +28,7 @@
         
     </head>
     <body class = 'bg-info'>
+        <!-- NAVIGATION icons -->
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
           <a class="navbar-brand" href="http://csumb.edu">CSUMB</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -35,10 +36,10 @@
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
-            <li class="nav-item active">
+            <li class="nav-item">
                 <a class="nav-link" href='index.php' id='home'>
                     <i class="fas fa-home"></i>
-                    Home<span class="sr-only">(current)</span> </a>
+                    Home</a>
             </li>
               <li class="nav-item">
                 <a class="nav-link" href="catalog.php" id = 'catalog'>
@@ -55,10 +56,10 @@
                     <i class="fas fa-info-circle"></i>
                     About Us</a>
               </li>
-                <li class="nav-item">
+                <li class="nav-item active">
                     <a class="nav-link" href='cart.php'>
                         <i class="fas fa-shopping-cart"></i>
-                        Cart: <?php displayCartCount(); ?> </a></li>
+                        Cart: <span class="sr-only">(current)</span><?php displayCartCount(); ?> </a></li>
                     </a>
                 </li>
             </ul>
