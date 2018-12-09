ad927aa33549119e0d59ef5a4b044603e6e4d480
chaco <hyada@csumb.edu>
2018-12-09 14:18:26 +0000

    3-5

--- api/getProductInfo.php
+++ api/getProductInfo.php
@@ -2,11 +2,13 @@
 
     include '../dbConnection.php';
     $dbConn = getDatabaseConnection("islandStore");    
-    $sql = "SELECT * 
-            FROM is_product 
-            WHERE id = :productId";
+    $sql = "SELECT * FROM is_product 
+            inner join is_category on is_product.categoryId = is_category.categoryId 
+            inner join is_type on is_product.typeId = is_type.typeId
+            WHERE is_product.productId = :productId";
+    // echo $sql;
     $stmt = $dbConn -> prepare($sql);
-    $stmt -> execute(array("id"=>$_GET['productId']));
+    $stmt -> execute(array("productId"=>$_GET['productId']));
     $resultSet = $stmt->fetch(PDO::FETCH_ASSOC);
     echo json_encode($resultSet);
         
